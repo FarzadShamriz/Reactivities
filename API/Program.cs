f2c7ec02bcf69ccd9631efb0741dfedc62a70180
Farzad Shamriz <farzadshamriz@outlook.com>
2026-01-13 18:02:53 -0500

    Added mediatR to application project and replaced get all activities endpoint with that

--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,4 @@
+using Application.Activities.Queries;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
 
@@ -9,15 +10,18 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddDbContext<AppDbContext>(o =>
 {
-   o.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+    o.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
 builder.Services.AddCors();
+builder.Services.AddMediatR(
+    x => x.RegisterServicesFromAssemblyContaining<GetActivityList.Handler>()
+    );
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000","https://localhost:3000"));
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000", "https://localhost:3000"));
 app.MapControllers();
 
 using var scope = app.Services.CreateScope();
@@ -28,10 +32,10 @@ try
     await context.Database.MigrateAsync();
     await DbInitializer.SeedData(context);
 }
-catch(Exception ex)
+catch (Exception ex)
 {
     var logger = services.GetRequiredService<ILogger<Program>>();
-    logger.LogError(ex,ex.Message);
+    logger.LogError(ex, ex.Message);
     throw;
 }
 
