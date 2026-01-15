11625cfc4f0b4e8592e5036d166a9ac3c03e06db
Farzad Shamriz <farzadshamriz@outlook.com>
2026-01-15 18:11:08 -0500

    Implemented Activity dashboard , item and navbar and dashboard

---
+++ Client/src/app/layout/App.tsx
@@ -0,0 +1,32 @@
+import { Container, CssBaseline } from "@mui/material";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Navbar from "./Navbar";
+import ActivityDashboard from "../../features/activities/Dashboard/ActivityDashboard";
+
+
+function App() {
+
+  const [activities, setActivities] = useState<Activity[]>([]);
+
+  useEffect(() => {
+
+    axios.get<Activity[]>('https://localhost:5001/api/activities')
+      .then(response => setActivities(response.data));
+
+    return () => { }
+  }, []);
+
+  return (
+    <>
+      <CssBaseline />
+      <Navbar />
+      <Container maxWidth='xl' sx={{mt: 3}}>
+        <ActivityDashboard activities={activities}/>
+      </Container>
+
+    </>
+  )
+}
+
+export default App
