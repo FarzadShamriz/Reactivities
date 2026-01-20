218d2866c02155c9216f0e81345c196d43439724
Farzad Shamriz <farzadshamriz@outlook.com>
2026-01-20 17:25:21 -0500

    Used mobx for client state management and added progress bar under the navbar (loading)

--- Client/src/app/layout/Navbar.tsx
+++ Client/src/app/layout/Navbar.tsx
@@ -1,16 +1,21 @@
 
 import { Group } from "@mui/icons-material"
-import { AppBar, Box, Container, MenuItem, Toolbar, Typography } from '@mui/material'
+import { AppBar, Box, Container, LinearProgress, MenuItem, Toolbar, Typography } from '@mui/material'
 import { NavLink } from "react-router"
 import MenuItemLink from "../shared/components/MenuItemLink"
+import { useStore } from "../../libs/hooks/useStore"
+import { Observer } from "mobx-react-lite"
 
 
 
 export default function Navbar() {
+    const { uiStore } = useStore();
+
     return (
         <Box sx={{ flexGrow: 1 }}>
             <AppBar position="static" sx={{
-                backgroundImage: 'linear-Gradient(135deg, #182a73 0%, #218aae 69%, #20a7ac 89%)'
+                backgroundImage: 'linear-Gradient(135deg, #182a73 0%, #218aae 69%, #20a7ac 89%)',
+                position:'relative'
             }}>
                 <Container maxWidth='xl'>
                     <Toolbar sx={{
@@ -31,12 +36,34 @@ export default function Navbar() {
                             <MenuItemLink to='/createActivity'>
                                 Create Activity
                             </MenuItemLink>
+
+
+                            <MenuItemLink to='/counter'>
+                                Counter
+                            </MenuItemLink>
                         </Box>
                         <MenuItem>
                             User Menu
                         </MenuItem>
                     </Toolbar>
                 </Container>
+
+
+                <Observer>
+                    {() => uiStore.isLoading ? (
+                        <LinearProgress
+                            color="secondary"
+                            sx={{
+                                position: 'absolute',
+                                bottom: 0,
+                                left: 0,
+                                right: 0,
+                                height: 4
+                            }}
+                        />
+                    ) : null}
+                </Observer>
+
             </AppBar>
         </Box>
     )
